d4febccd5ba5455bf5df465804942d48ebc598ab
Erdem Inan <erdem.inan@aproplan.com>
2019-04-09 11:07:16 +0200

    Migrate code to swift 5 and Xcode 10.2.

--- Source/SideMenuController.swift
+++ Source/SideMenuController.swift
@@ -36,7 +36,7 @@ public extension SideMenuController {
      Toggles the side pannel visible or not.
      */
     @objc
-    public func toggle() {
+    func toggle() {
         
         if !transitionInProgress {
             if !sidePanelVisible {
@@ -58,7 +58,7 @@ public extension SideMenuController {
      
      - returns: Cached UIViewController or nil
      */
-    public func viewController(forCacheIdentifier identifier: String) -> UIViewController? {
+    func viewController(forCacheIdentifier identifier: String) -> UIViewController? {
         return controllersCache[identifier]
     }
     
@@ -67,7 +67,7 @@ public extension SideMenuController {
      
      - parameter sideViewController: controller to be embedded
      */
-    public func embed(sideViewController controller: UIViewController) {
+    func embed(sideViewController controller: UIViewController) {
         if sideViewController == nil {
             
             sideViewController = controller
@@ -88,7 +88,7 @@ public extension SideMenuController {
      - parameter centerViewController: controller to be embedded
      - parameter cacheIdentifier: identifier for the view controllers cache
      */
-    public func embed(centerViewController controller: UIViewController, cacheIdentifier: String? = nil) {
+    func embed(centerViewController controller: UIViewController, cacheIdentifier: String? = nil) {
         
         guard controller !== centerViewController else {
             if sidePanelVisible {
